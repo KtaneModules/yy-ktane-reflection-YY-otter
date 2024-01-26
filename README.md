f6884de26665f689a54303ba83d8545c7ccff225
KtaneModulesBot <ktanemodulesbot@gmail.com>
2024-01-26 17:57:36 +0100

    Add workflow branch files

---
+++ README.md
@@ -0,0 +1,5 @@
+# [yy-ktane-reflection (By YY-otter)](https://github.com/YY-otter/yy-ktane-reflection)
+
+Unfortunately the way GitHub triggers workflows doesn't allow them to run on a schedule on a non-default branch. So this branch (updater_workflow) has to be the default. Switch to a different branch to see the source code of the mod (for ex. master -> the master branch of the mod)
+
+To recover lost commits, use the commit hashes from [GitHub's reflog](https://api.github.com/repos/KtaneModules/yy-ktane-reflection-YY-otter/events) (add `?per_page=#&page=#` to the log URL to increase the page size (maximum is 100, default is 30) and/or specify the page number (default is 1)).
---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+git config user.name $USERNAME
+git config user.email $EMAIL
+git config checkout.defaultRemote origin
+git fetch origin
+git checkout master
+git remote add upstream https://github.com/YY-otter/yy-ktane-reflection
+git fetch upstream
+if [ $? -ne 0 ]; then
+	echo "Failed to fetch the original repo, exiting..."
+	exit 128
+fi
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 1; done
+  return 0
+}
+
+ORIGIN=()
+UPSTREAM=()
+
+for branch in $(git branch -r | tr "\n" "\n"); do
+  if [[ "$branch" != "->" && "$branch" != "origin/HEAD" && "$branch" != "upstream/updater_workflow" ]]; then
+    ARR=($( cut -d '/' -f 1 <<< "$branch" ) $( cut -d '/' -f 2- <<< "$branch" ))
+    if [[ "${ARR[0]}" == "origin" ]]; then
+        ORIGIN+=("${ARR[1]}")
+    else
+        UPSTREAM+=("${ARR[1]}")
+    fi
+  fi
+done
+
+for branch in "${UPSTREAM[@]}"; do
+    echo "Preparing $branch"
+    containsElement "$branch" "${ORIGIN[@]}"
+    if [ $? -eq 0 ]; then
+        git switch --orphan $branch
+    else
+        git checkout $branch
+    fi
+    echo "Pushing $branch"
+    git reset --hard upstream/$branch
+    git push -f https://$USERNAME:$TOKEN@github.com/$REPOSITORY.git $branch
+done
